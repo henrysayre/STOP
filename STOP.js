86892a2093ccac03316c28d55eeb6674ed1afb6d
Isaac Raya <isaacraya@students.berkeley.net>
2018-05-16 11:36:23 -0700

    hi

--- STOP.js
+++ STOP.js
@@ -1,2 +1,11 @@
-function goals(){
+var totalPoints = 0;
+
+function goal {
+    this.goalName = goalName;
+    this.numPoints = numPoints;
+    this.goalDescip = goalDescrip;
+}
+
+function points(goalName, numPoints){
+
 }
