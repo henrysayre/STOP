29a85baf67c6f13616787313d1c4d8ed261cb226
Isaac Raya <isaacraya@students.berkeley.net>
2018-05-18 10:44:31 -0700

    hi

--- STOP.js
+++ STOP.js
@@ -1,4 +1,7 @@
 var totalPoints = 0;
+var allGoals = [goals];
+var prize = "";
+
 
 function goal {
     this.goalName = goalName;
@@ -7,5 +10,14 @@ function goal {
 }
 
 function points(goalName, numPoints){
+    for(var i = 0; i < allGoals.length; i++ ){
+        //if goal is completed
+        totalPoints += numPoints;
+    }
+}
 
+function prizes(numPoints){
+    if(totalPoints == 100){
+        //set html element to return prize
+    }
 }
