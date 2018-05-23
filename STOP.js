cf3f42251c5d818e65216dbf694ad68016cc4b50
Isaac Raya <isaacraya@students.berkeley.net>
2018-05-23 10:45:17 -0700

    hi

--- STOP.js
+++ STOP.js
@@ -1,19 +1,31 @@
+var id = 0;
 var totalPoints = 0;
-var allGoals = [goals];
+var allGoals = [new goal("15 minute nature walk",20, "Going on a 15 minute(or longer) walk in nature", false), new goal()];
 var prize = "";
 
 
-function goal {
+function goal( goalName, numPoints, goalDescrip) {
+    this.id = id++;
     this.goalName = goalName;
     this.numPoints = numPoints;
     this.goalDescip = goalDescrip;
+    this.completed = false;
 }
 
 function points(goalName, numPoints){
     for(var i = 0; i < allGoals.length; i++ ){
-        //if goal is completed
-        totalPoints += numPoints;
+        if(allGoals[i] == true){
+            totalPoints += allGoals[i].numPoints;
+        }
     }
+}
+
+function completeGoal(goalId) {
+    for(var i = 0; i < allGoals.length; i++){
+
+    }
+
+    //iterate through aLL GOALS AND MARK THE ONE COMPLETE THAT THEY COMPLKTER
 }
 
 function prizes(numPoints){
